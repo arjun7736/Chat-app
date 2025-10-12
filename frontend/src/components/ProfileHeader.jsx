69c093150c5353a73ac2b3ee7f0ac10b499efd81
arjun7736 <arjunvengassery123@gmail.com>
2025-10-12 16:57:03 +0530

    Feate : ChatPage Layout completed

---
+++ frontend/src/components/ProfileHeader.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { useAuthStore } from '../store/useAuthStore'
+
+const ProfileHeader = () => {
+      const{logOut}=useAuthStore()
+
+  return (
+    <div className="p-6 border-b border-slate-700/50">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3">
+
+          <div className="avatar online">
+            <button
+              className="size-14 rounded-full overflow-hidden relative group"
+              onClick={() => fileInputRef.current.click()}
+            >
+              <img
+                src={selectedImg || authUser.profilePic || "/avatar.png"}
+                alt="User image"
+                className="size-full object-cover"
+              />
+              <div className="absolute inset-0 bg-black/50 opacity-0 group-hover:opacity-100 flex items-center justify-center transition-opacity">
+                <span className="text-white text-xs">Change</span>
+              </div>
+            </button>
+
+            <input
+              type="file"
+              accept="image/*"
+              ref={fileInputRef}
+              onChange={handleImageUpload}
+              className="hidden"
+            />
+          </div>
+
+          <div>
+            <h3 className="text-slate-200 font-medium text-base max-w-[180px] truncate">
+              {authUser.fullName}
+            </h3>
+
+            <p className="text-slate-400 text-xs">Online</p>
+          </div>
+        </div>
+
+
+        <div className="flex gap-4 items-center">
+
+          <button
+            className="text-slate-400 hover:text-slate-200 transition-colors"
+            onClick={logOut}
+          >
+            <LogOutIcon className="size-5" />
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProfileHeader
---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,42 @@
+import {create} from "zustand"
+import { axiosInstance } from "../lib/axios"
+import toast from "react-hot-toast"
+
+export const useChatStore=create((set)=>({
+    allContracts:[],
+    chats:[],
+    messages:[],
+    activeTab:"chats",
+    selectedUser:null,
+    isUsersLoading:false,
+    isMessagesLoading:false,
+
+
+
+    setActivetab:(tab)=>set({activeTab:tab}),
+    setSelectedUser:(selectedUser)=>set({selectedUser}),
+
+    getAllContacts:async()=>{
+        set({isUsersLoading:true})
+        try {
+            const res = await axiosInstance.get("/message/contracts")
+            set({allContracts:res.data})
+        } catch (error) {
+            toast.error(error.response.data.message)
+        }finally{
+            set({isUsersLoading:false})
+        }
+    },
+
+    getMyChatPartners:async()=>{
+         set({isUsersLoading:true})
+        try {
+            const res = await axiosInstance.get("/message/chats")
+            set({chats:res.data})
+        } catch (error) {
+            toast.error(error.response.data.message)
+        }finally{
+            set({isUsersLoading:false})
+        }
+    }
+}))
