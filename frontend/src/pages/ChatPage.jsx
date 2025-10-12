ba23b7b3007175ace0cec4ecda93dbdcf9ef8b4b
arjun7736 <arjunvengassery123@gmail.com>
2025-10-12 16:21:02 +0530

    Feate : Login and Logout completed

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { useAuthStore } from '../store/useAuthStore'
 
 const ChatPage = () => {
+  const{logOut}=useAuthStore()
   return (
-    <div>ChatPage</div>
+    <div className='z-10'>
+      <button onClick={logOut} className='z-10'>logOut</button>
+    </div>
     
   )
 }
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -6,6 +6,7 @@ export const useAuthStore = create((set) => ({
   authUser: null,
   isCheckingAuth: true,
   isSignup:false,
+  isLoggingin:false,
 
 
   checkAuth: async () => {
@@ -32,6 +33,33 @@ export const useAuthStore = create((set) => ({
       toast.error(error.resposnse.data.message)
     }finally{
       set({isSignup:false})
+    }
+  },
+
+
+  login:async(data)=>{
+    set({isLoggingin:true})
+    try {
+       const res = await axiosInstance.post("/auth/login",data)
+       set({authUser:res.data})
+
+       toast.success("Logged In")
+    } catch (error) {
+      console.log(error);
+      toast.error(error.resposnse.data.message)
+    }finally{
+      set({isLoggingin:false})
+    }
+  },
+
+  logOut:async()=>{
+    try {
+      await axiosInstance.post("/auth/logout")
+      set({authUser:null})
+      toast.success("Loggedout")
+    } catch (error) {
+      toast.error("Error in Logout")
+      console.log(error);
     }
   }
 }));
