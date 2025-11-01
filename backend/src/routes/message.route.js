85d5c7769958053b869d06db7e0c5ddf23f00418
arjun7736 <arjunvengassery123@gmail.com>
2025-11-01 10:04:53 +0530

    [Test] Testing CI/CD pipeline

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,16 +1,19 @@
-import express from "express"
-import { getAllContacts, getChatPartners, getMessagesByUserId, sendMessage } from "../controllers/message.controller.js"
-import { protectRoute } from "../middleware/auth.middleware.js"
+import express from "express";
+import {
+  getAllContacts,
+  getChatPartners,
+  getMessagesByUserId,
+  sendMessage,
+} from "../controllers/message.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
-const router =express.Router()
+const router = express.Router();
 
-router.use(protectRoute)
+router.use(protectRoute);
 
+router.get("/contracts", getAllContacts);
+router.get("/chats", getChatPartners);
+router.get("/:id", getMessagesByUserId);
+router.post("/send/:id", sendMessage);
 
-router.get("/contracts",getAllContacts)
-router.get("/chats",getChatPartners)
-router.get("/:id",getMessagesByUserId)
-router.post("/send/:id",sendMessage)
-
-
-export default router   
+export default router;
